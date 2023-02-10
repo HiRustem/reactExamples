7c90b20e3a71bdd60a38527398c75d3a10d808d8
HiRustem <87270748+HiRustem@users.noreply.github.com>
2023-02-10 10:09:18 +0300

    ReactExamples

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      rotate: {
+        '16': '16deg',
+        '11': '11deg',
+        '32': '32deg',
+        '40': '40deg',
+        '64': '64deg',
+        '65': '68deg',
+        '170': '170deg',
+      },
+    },
+  },
+  plugins: [],
+}
